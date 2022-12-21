320850333d1884fd39bcb2f696829ff243c0dd27
Junella Caringal <junellacaringal@gmail.com>
2022-12-21 11:56:53 -0800

    minor css updates & remove hirenav mobile function

--- static/navbar.js
+++ static/navbar.js
@@ -1,25 +1,25 @@
 // When the user scrolls down, hide the navbar. When the user scrolls up, show the navbar */
-var prevScrollpos = window.pageYOffset; 
+let prevScrollpos = window.pageYOffset; 
 window.onscroll = function() {
     let currentScrollPos = window.pageYOffset;   
     if (prevScrollpos > currentScrollPos) {
         // if mobile, hide navbar on scroll
-        if (window.innerWidth < 768) {
-            document.getElementById("hireMeLink").style.bottom = "-75px";
-        } else {
+        // if (window.innerWidth < 768) {
+        //     document.getElementById("hireMeLink").style.bottom = "-75px";
+        // } else {
              // document.getElementById("navbar").style.top = "0";
             document.getElementById("hireMeLink").style.top = "-20%";
             document.getElementById("hireMeLink").style.opacity = "0";
-        }
+        // }
     } else {
-        if (window.innerWidth < 768) {
-            document.getElementById("hireMeLink").style.bottom = "0";
-            document.getElementById("hireMeLink").style.transition = "top .5s";
-        } else {
+        // if (window.innerWidth < 768) {
+        //     document.getElementById("hireMeLink").style.bottom = "0";
+        //     document.getElementById("hireMeLink").style.transition = "bottom .5s";
+        // } else {
             // document.getElementById("navbar").style.top = "-75px";
             document.getElementById("hireMeLink").style.top = "0"
             document.getElementById("hireMeLink").style.opacity = "1";
-        }
+        // }
     }
     prevScrollpos = currentScrollPos;
     }
